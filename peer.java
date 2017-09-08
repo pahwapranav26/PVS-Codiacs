9f35b091562e44017d9dcaf1ccf3aa3abc556ecc
pahwapranav26 <pahwapranav26@users.noreply.github.com>
2017-09-08 19:56:36 +0530

    Add files via upload

---
+++ peer.java
@@ -0,0 +1,58 @@
+import java.io.*
+class peer
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader (new InputStreamReader(System.in))
+String t_s,name,ch;
+PrintWriter pw = new PrintWriter(new File("File.csv"));
+StringBuilder sb = new StringBuilder();
+sb.append("Teacher/Student");
+sb.append(',');
+sb.append("Name");
+sb.append(',');
+sb.append("Subject");
+sb.append(',');
+sb.append("Teach/Study");
+sb.append('\n');
+System.out.println("Are you a Teacher or Student");
+System.out.println("T-- for teacher,,, S for student.");
+t_s=br.readLine();
+System.out.println("Enter Your Name");
+name=br.readLine();
+System.out.println("Enter Your Subject");
+subject=br.readLine();
+if(t_s=="T")
+{
+	
+	sb.append("Teacher");
+	sb.append(',');
+        sb.append("name");
+        sb.append(',');
+        sb.append("subject");
+        sb.append(',');
+	sb.append("---");
+	sb.append('\n');
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("done!");
+}
+else if(t_s=="s")
+{
+	System.out.println("T.Teach___ H.Help in studies");
+	ch=br.readLine();
+	sb.append("Teacher");
+	sb.append(',');
+        sb.append("name");
+        sb.append(',');
+        sb.append("subject");
+        sb.append(',');
+	sb.append("ch");
+	sb.append('\n');
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("done!");
+}
+else
+	System.out.println("Invalid Choice");
+System.out.println("We will Get back to You");
